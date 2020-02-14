5e91fd9a2e1992279218dfffdb66f2ca68fd26f3
Sydney Crumley <srsimps19@gmail.com>
2020-02-14 10:37:07 -0600

    finished ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,9 +9,13 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    for i in range(length):
+        compare = hash_table_retrieve(ht, limit - weights[i])
+        if compare is None:
+            hash_table_insert(ht, weights[i], i)
+        else:
+            answer = (i, compare)
+            return answer
 
     return None
 
